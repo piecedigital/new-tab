3b1c2ac734ac96876963d00b0d218b7c9e0ea290
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-06-04 21:08:46 -0400

    added new tabs

--- js/angmod.js
+++ js/angmod.js
@@ -9,5 +9,8 @@ angular.module("listApp", [])
 		{text: "FCC Slack", url: "http://freecode.slack.com/"},
 		{text: "Code Newbie Slack", url: "http://codenewbie.slack.com/"},
 		{text: "Github", url: "http://github.com/"},
-		{text: "Linkedin", url: "http://linkedin.com/"}];
+		{text: "Linkedin", url: "http://linkedin.com/"},
+		{text: "Free Code Camp", url: "http://freecodecamp.com/"},
+		{text: "GMail - PDS", url: "hhttps://mail.google.com/mail/u/0/"},
+		{text: "GMail - Trash", url: "hhttps://mail.google.com/mail/u/1/"}];
 	});
